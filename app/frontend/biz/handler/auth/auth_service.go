57344c58d901591532578dfce2b624da5a7ab24a
kyzyc <yuchenzhu66@gmail.com>
2025-01-12 10:40:25 +0800

    feat: code generated by cwgo.

--- app/frontend/biz/handler/auth/auth_service.go
+++ app/frontend/biz/handler/auth/auth_service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/kyzyc/biz-demo/gomall/app/frontend/biz/service"
 	"github.com/kyzyc/biz-demo/gomall/app/frontend/biz/utils"
 	auth "github.com/kyzyc/biz-demo/gomall/app/frontend/hertz_gen/frontend/auth"
+	common "github.com/kyzyc/biz-demo/gomall/app/frontend/hertz_gen/frontend/common"
 )
 
 // Login .
@@ -53,4 +54,24 @@ func Register(ctx context.Context, c *app.RequestContext) {
 	//utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 
 	c.Redirect(consts.StatusOK, []byte("/"))
+}
+
+// Logout .
+// @router /auth/logout [POST]
+func Logout(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req common.Empty
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	resp, err := service.NewLogoutService(ctx, c).Run(&req)
+
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+	utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 }
