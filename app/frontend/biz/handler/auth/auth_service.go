10aeb4a0b37f7e3b91a336931b2229a63c6517dd
kyzyc <yuchenzhu66@gmail.com>
2025-01-13 18:54:41 +0800

    feat: basic frontend login logic.

--- app/frontend/biz/handler/auth/auth_service.go
+++ app/frontend/biz/handler/auth/auth_service.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"context"
-
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/kyzyc/biz-demo/gomall/app/frontend/biz/service"
@@ -22,13 +21,13 @@ func Login(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	_, err = service.NewLoginService(ctx, c).Run(&req)
+	redirect, err := service.NewLoginService(ctx, c).Run(&req)
 	if err != nil {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
 		return
 	}
 
-	c.Redirect(consts.StatusOK, []byte("/"))
+	c.Redirect(consts.StatusOK, []byte(redirect))
 
 	//utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 	//utils.SendSuccessResponse(ctx, c, consts.StatusOK, "done!")
--- app/frontend/biz/service/login.go
+++ app/frontend/biz/service/login.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/cloudwego/hertz/pkg/app"
 	auth "github.com/kyzyc/biz-demo/gomall/app/frontend/hertz_gen/frontend/auth"
-	common "github.com/kyzyc/biz-demo/gomall/app/frontend/hertz_gen/frontend/common"
 )
 
 type LoginService struct {
@@ -18,7 +17,7 @@ func NewLoginService(Context context.Context, RequestContext *app.RequestContext
 	return &LoginService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *LoginService) Run(req *auth.LoginReq) (resp *common.Empty, err error) {
+func (h *LoginService) Run(req *auth.LoginReq) (redirect string, err error) {
 	//defer func() {
 	// hlog.CtxInfof(h.Context, "req = %+v", req)
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
@@ -27,5 +26,9 @@ func (h *LoginService) Run(req *auth.LoginReq) (resp *common.Empty, err error) {
 	session := sessions.Default(h.RequestContext)
 	session.Set("user_id", 1)
 	err = session.Save()
-	return
+	redirect = "/"
+	if req.Next != "" {
+		redirect = req.Next
+	}
+	return redirect, nil
 }
