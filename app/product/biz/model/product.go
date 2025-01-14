1342d1d808f581ec59f122d2300df903a7025dd6
kyzyc <yuchenzhu66@gmail.com>
2025-01-14 21:15:43 +0800

    feat: add product model.

---
+++ app/product/biz/model/product.go
@@ -0,0 +1,15 @@
+package model
+
+type Product struct {
+	Base
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Picture     string  `json:"picture"`
+	Price       float32 `json:"price"`
+
+	Categories []Category `json:"categories" gorm:"many2many:product_categories;"`
+}
+
+func (p Product) TableName() string {
+	return "product"
+}
